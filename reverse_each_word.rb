c55ad0b4cdc28eddd16e9bc820735c1254afc45c
Roman Turner <rn.turner@live.com>
2021-01-21 00:28:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (str)
  new_array = str.split(" ") 
- new_array.each do 
-   new_array.reverse_each {|word| word << new_array}
+ new_array.each do |word| 
+   new_array.reverse
  end 
  new_array.join(" ")
  new_array
