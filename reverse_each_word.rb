61a314cbc8b6ca344e05395a73feb7ed92802b6f
Roman Turner <rn.turner@live.com>
2021-01-21 00:42:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def reverse_each_word (str)
- str = str.split(' ')
  
- str.each do |word|
+ str1 = str.split(' ')
+ str1.each do |word|
    word.split(//).reverse
    end 
-  str
+  str1
 end
